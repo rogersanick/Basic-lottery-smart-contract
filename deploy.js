0704ba6de6598e37efe946a63cb1ed80d9673648
rogersanick <rogers.a.nick@gmail.com>
2018-08-31 13:24:44 -0400

    Adding functionality to deploy to Rinkeby network

--- deploy.js
+++ deploy.js
@@ -4,5 +4,19 @@ require('dotenv').config();
 const { interface, bytecode } = require('./compile.js');
 
 const provider = new HDWalletProvider(
-  process.env.mnemonic
+  process.env.mnemonic,
+  'https://rinkeby.infura.io/v3/cff505b264bb479b9df907c80ee66f5f'
 );
+
+const web3 = new Web3(provider);
+
+(async () => {
+  const accounts = await web3.eth.getAccounts();
+  console.log('Attempting to deploy from account ', accounts[0]);
+
+  const result = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: '0x' + bytecode, arguments: ['HEYHEYYOUYOUIDONTLIKEYOURGIRLFRIEND'] })
+    .send({ gas: '5000000', from: accounts[0] });
+
+  console.log('Contract deployed to ', result.options.address);
+})();
